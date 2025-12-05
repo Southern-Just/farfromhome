4d6082538150bf11a0690ef87caa26d6b3d9c8c2
Southern Just <karuguezekiel@gmail.com>
2025-12-05 19:43:09 +0300

    farfromhome 001ffm

---
+++ app/(hero)/page.tsx
@@ -0,0 +1,28 @@
+const Hero = () => {
+  return (
+    <main>
+      <section
+        className="relative h-screen bg-cover bg-center bg-no-repeat "
+        style={{ backgroundImage: "url('/icons/hero_img.svg')",
+          opacity: 0.7
+         }}
+      >
+        <div className="absolute container top-0 left-0 w-full p-6">
+          <p>header</p>
+        </div>
+        <div className="h-full flex flex-col justify-center items-start px-6 space-y-4 ml-6 text-foreground">
+          <h1 className="text-5xl font-bold">Toka kwenu, Tukatembee</h1>
+
+          <p className="max-w-xl ml-6">
+            Personalize your trip in minutes—choose your destination, set your
+            interests, and explore with ease.
+          </p>
+
+          <button className="ml-4 sm:px-10">Twende</button>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Hero;
