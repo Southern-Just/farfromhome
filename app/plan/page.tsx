f45b77077d7f30cc75320c34a3248e0cf45abcd8
Southern Just <117814631+Southern-Just@users.noreply.github.com>
2025-12-11 22:13:42 +0300

    ui | ux product second

--- app/plan/page.tsx
+++ app/plan/page.tsx
@@ -5,14 +5,14 @@ const plan = () => {
   return (
     <div >
       <Header />
-      <div style={{ backgroundImage: "url('/images/confeti.png')" }}>
-        <div className="text-4xl font-bold flex flex-col justify-center space-y-4 mt-16">
+      <div style={{ backgroundImage: "url('/images/confeti.png')" }} className="items-center space-y-6 md:space-y-14 mt-6">
+        <div className="text-4xl font-bold flex flex-col justify-center space-y-4 ">
           <h1 className="flex justify-center">Create Your Travel Plan</h1>
           <p className="text-xs font-normal text-gray-500 flex justify-center">
             Personalise your very own : To your Dream Destination
           </p>
         </div>
-        <section className="w-full h-screen flex items-center justify-center ">
+        <section className="w-full h-screen flex justify-center ">
           <div className=" space-x-4 rounded-sm outline-0 w-[80%]">
             <Plan />
           </div>
