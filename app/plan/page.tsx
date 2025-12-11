802d758e0eafa97789e6696a96785bff56da7b6f
Southern Just <117814631+Southern-Just@users.noreply.github.com>
2025-12-11 23:41:37 +0300

    ui | ux product second, 2.1

--- app/plan/page.tsx
+++ app/plan/page.tsx
@@ -1,19 +1,28 @@
 import Header from "@/components/Header";
 import Plan from "@/components/Plan";
 
-const plan = () => {
+const PlanPage = () => {
   return (
-    <div >
+    <div>
       <Header />
-      <div style={{ backgroundImage: "url('/images/confeti.png')" }} className="items-center space-y-6 md:space-y-14 mt-6">
-        <div className="text-4xl font-bold flex flex-col justify-center space-y-4 ">
-          <h1 className="flex justify-center">Create Your Travel Plan</h1>
+
+      <div
+        style={{ backgroundImage: "url('/images/confeti.png')" }}
+        className="items-center space-y-6 md:space-y-14 mt-6"
+      >
+        <div className="text-4xl font-bold flex flex-col justify-center space-y-4">
+          
+          <h1 className="typewriter mx-auto">
+            Create Your Travel Plan
+          </h1>
+
           <p className="text-xs font-normal text-gray-500 flex justify-center">
             Personalise your very own : To your Dream Destination
           </p>
         </div>
-        <section className="w-full h-screen flex justify-center ">
-          <div className=" space-x-4 rounded-sm outline-0 w-[80%]">
+
+        <section className="w-full h-screen flex justify-center">
+          <div className="space-x-4 rounded-sm outline-0 w-[80%]">
             <Plan />
           </div>
         </section>
@@ -21,4 +30,5 @@ const plan = () => {
     </div>
   );
 };
-export default plan;
+
+export default PlanPage;
